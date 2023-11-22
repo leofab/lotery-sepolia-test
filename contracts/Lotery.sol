f252df407567f2c7ca64deb74d732e7bf81e190f
Leo Fabricio <leofabr3@gmail.com>
2023-11-22 10:16:44 -0300

    tests failures continue but solc is now working

--- contracts/Lotery.sol
+++ contracts/Lotery.sol
@@ -1,52 +1,39 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.25;
 
 contract Lotery {
     address public manager;
     address[] public players;
 
-    constructor() {
+    constructor() public {
         manager = msg.sender;
     }
 
-    function enter() public payable {
-        require(msg.value > 0.1 ether);
+    function getManager() public view returns (address) {
+        return manager;
+    }
 
+    function enter() public payable {
+        require(msg.value > 0.01 ether);
         players.push(msg.sender);
     }
 
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
+
     function random() private view returns (uint) {
-        return
-            uint(
-                keccak256(
-                    abi.encode(block.difficulty, block.timestamp, players)
-                )
-            );
+        return uint(keccak256(abi.encode(block.difficulty, now, players)));
     }
 
     function pickWinner() public restricted {
-        // uint index = random() % players.length;
-        // payable(players[index]).transfer(address(this).balance);
-        // address payable winner = payable(players[index]);
-        // uint contractBalance = address(this).balance;
-
-        // winner.transfer(contractBalance);
-        // players = new address[](0);
-
         uint index = random() % players.length;
-        address winner = players[index];
-        uint contractBalance = address(this).balance;
-
-        winner.transfer(contractBalance);
+        players[index].transfer(address(this).balance);
         players = new address[](0);
     }
 
     modifier restricted() {
         require(msg.sender == manager);
         _;
-    }
-
-    function getPlayers() public view returns (address[] memory) {
-        return players;
     }
 }
--- test/Lotery.test.js
+++ test/Lotery.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const ganache = require('ganache');
 const { Web3 } = require('web3');
 const provider = ganache.provider();
-const web3 = new Web3(provider);
+const web3 = new Web3(provider, null, { transactionConfirmationBlocks: 1 });
 const { interface, bytecode } = require('../compile');
 
 let lotery;
@@ -10,14 +10,26 @@ let accounts;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
-
     lotery = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode })
-        .send({ from: accounts[0], gas: 1000000 });
+        .send({ from: '0x' + accounts[0], gas: '1000000' });
 });
 
 describe('Lotery Contract', () => {
-    it('deploys a contract', () => {
-        assert.ok(lotery.options.address);
+    it('allows one account to enter', async () => {
+        await lotery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        }).then(() => {
+            lotery.methods.getPlayers().call({
+                from: accounts[0]
+            }).then((players) => {
+                assert.equal(accounts[0], players[0]);
+                assert.equal(1, players.length);
+                done();
+            }).catch((error) => {
+                done(error);
+            });
+        });
     })
 })
